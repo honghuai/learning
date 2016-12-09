258f5d6758c3a5e6420384d370f3137ae79b25bd
fanhonghuai <honghuai@users.noreply.github.com>
2016-12-09 18:14:13 +0800

    vue2.js基础API学习
    Vue2.js基础知识学习。标注有"疑惑"字样的，闲暇时间再花心思琢磨。
    http://cn.vuejs.org/v2/guide/instance.html

---
+++ vue2-base/src/vue/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>
+        {{greeting}}world!
+    </p>
+</template>
+
+<script>
+    moudle.exports = {
+        data: function() {
+            return {
+                greeting: "Helllo"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+p{
+    font-size:2em;
+    text-align: center;
+}
+</style>
